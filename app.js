484998729b83fd67ee3471072364e0a00b0cf433
Aaron Board <ajboard13@gmail.com>
2019-12-18 10:46:15 -0500

    addding 404 conditional

--- app.js
+++ app.js
@@ -13,13 +13,18 @@ http.createServer(function(req, res) {
     }
 
     //if the endpoint is localhost:1337/api return data.
-    if (req.url === '/api'){
+    else if (req.url === '/api'){
         res.writeHead(200, {'Content-Type' : 'application/json'});
         var obj = {
             firstname: 'John',
             lastname: 'Doe'
         };
         res.end(JSON.stringify(obj));
+    }
+
+    else {
+        res.writeHead(404);
+        res.end();
     }
 
 }).listen(1337, '127.0.0.1');
