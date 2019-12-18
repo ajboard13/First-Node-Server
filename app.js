167f62b5799f34a09068ea6dc7fbb6d21348fb77
Aaron Board <ajboard13@gmail.com>
2019-12-18 10:15:22 -0500

    making the simplest of simple APIs

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
 
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
-    var message = 'hello world...';
-    html = html.replace('{message}', message);
-    res.end(html);
+    res.writeHead(200, {'Content-Type' : 'application/json'});
+    var obj = {
+        firstname: 'John',
+        lastname: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
+
 
 }).listen(1337, '127.0.0.1');
