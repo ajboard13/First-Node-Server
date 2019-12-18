b94993117e0213c1c5fa5b53586530359ef2fc57
Aaron Board <ajboard13@gmail.com>
2019-12-18 10:26:44 -0500

    adding an example of endpoints.

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
 
-    res.writeHead(200, {'Content-Type' : 'application/json'});
-    var obj = {
-        firstname: 'John',
-        lastname: 'Doe'
-    };
-    res.end(JSON.stringify(obj));
+    //if the endpoint is '/' pipe back the html file.
+    if (req.url === '/'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        //we use streams here to increase performance.
+        //otherwise if we store the contents of index.htm to a variable and pass it back
+        // it would increase the buffer size. Since res is a writable stream, we can pipe in a readable stream to save memory.
+        fs.createReadStream(__dirname + '/index.htm').pipe(res);
+    }
 
+    //if the endpoint is localhost:1337/api return data.
+    if (req.url === '/api'){
+        res.writeHead(200, {'Content-Type' : 'application/json'});
+        var obj = {
+            firstname: 'John',
+            lastname: 'Doe'
+        };
+        res.end(JSON.stringify(obj));
+    }
 
 }).listen(1337, '127.0.0.1');
